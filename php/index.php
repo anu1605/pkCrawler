4a95b8b7360c335f85a3f8640ec572e34d94883d
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-05-03 17:41:06 +0530

    first commit

---
+++ php/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// print_r(explode('<div class="col-12 sm-col-4 md-col-4 card-box">', file_get_contents("https://epaper.prabhatkhabar.com/")));
+// echo explode('<div class="col-md-12">', file_get_contents("https://epaper.prabhatkhabar.com/"))[1];
+echo file_get_contents("https://epaper.prabhatkhabar.com/t/18525");
---
+++ python/app.py
@@ -0,0 +1,8 @@
+import requests
+# url
+# https://epaper.prabhatkhabar.com/t/18525-Garhwa
+# https://epaper.prabhatkhabar.com/t/6185-RANCHI - City
+# https://epaper.prabhatkhabar.com/t/18526- Hazaribagh
+# https://epaper.prabhatkhabar.com/t/18527-Gumla
+# https://epaper.prabhatkhabar.com/t/18528-Koderma
+print(requests.get("https://epaper.prabhatkhabar.com/t/18525").text)
